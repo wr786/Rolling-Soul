b44e8130a99523647d246571d1c5a06ef26121e1
wr786 <wr786@pku.edu.cn>
2020-07-03 17:37:06 +0800

    添加了bgm

--- src/main.py
+++ src/main.py
@@ -14,6 +14,7 @@ HEIGHT = 851
 floors = {}
 walls = {}
 wallSize = 37	# 一个方块的大小
+level = 1	# 现在是第几关
 
 # 背景相关
 floorcnt = 0
@@ -306,6 +307,9 @@ def draw():
 	if roleChoose == 0:
 		start_view()
 	else:
+		if level == 1 and not music.is_playing('bgm_boss'):	# 仅用来测试，必然要调整
+			music.play('bgm_boss')
+			music.set_volume(0.7)
 		draw_map()
 		player.actor.draw()
 		player.weapon.actor.draw()
